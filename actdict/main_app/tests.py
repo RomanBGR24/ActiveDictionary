ff83a37a54221fe881fa9661b61f79996e6dda7d
RomanBGR24 <62135606+RomanBGR24@users.noreply.github.com>
2021-09-20 19:35:40 +0300

    first module test

--- actdict/main_app/tests.py
+++ actdict/main_app/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class Main_app(TestCase):
+    def test_index_page_returns_correct_html(self):
+        response = self.client.get('/')
+
+        self.assertTemplateUsed(response, 'index.html')
